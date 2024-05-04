e6482b69dfad9097b0a5d83fe8f77868e1f294ae
Gustas <loggustas@gmail.com>
2024-05-04 21:33:11 +0300

    New branch

---
+++ MeallyExtended.API/Controllers/RecipeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeallyExtended.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RecipeController : ControllerBase
+    {
+
+       //[HttpPost(Name = "CreateRecipe")]
+       // public IActionResult Get()
+       // {
+            
+       // }
+    }
+}
