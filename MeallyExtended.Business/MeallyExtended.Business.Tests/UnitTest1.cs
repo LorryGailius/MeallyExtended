7a42a8932d8e1c9ba6f6c6e4f05b16c7525db7d1
Gustas <loggustas@gmail.com>
2024-05-04 22:46:24 +0300

    Unit tests project

---
+++ MeallyExtended.Business/MeallyExtended.Business.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace MeallyExtended.Business.Tests
+{
+    
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TestMapper1()
+        {
+
+        }
+    }
+}
