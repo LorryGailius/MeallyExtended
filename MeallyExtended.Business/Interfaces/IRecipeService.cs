89385714262db02b6c19d2ea3bd1408aac308322
Gustas <loggustas@gmail.com>
2024-05-05 12:30:54 +0300

    Define example service

---
+++ MeallyExtended.Business/Interfaces/IRecipeService.cs
@@ -0,0 +1,25 @@
+﻿using MeallyExtended.Contracts.Dto;
+using MeallyExtended.DataModels.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeallyExtended.Business.Interfaces
+{
+    public interface IRecipeService
+    {
+        public RecipeDto GetRecipeById(Guid recipeId);
+
+        public bool AddRecipe(RecipeDto recipe);
+
+        public bool UpdateRecipe(RecipeDto recipe);
+
+        public bool DeleteRecipe(Guid recipeId);
+
+        public IEnumerable<RecipeDto> GetRecipesByCategory(List<CategoryDto> categories);
+
+        public RecipeDto GetRecipeByTitle(string title);
+    }
+}
---
+++ MeallyExtended.Business/Services/RecipeService.cs
@@ -0,0 +1,43 @@
+﻿using MeallyExtended.Business.Interfaces;
+using MeallyExtended.Contracts.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeallyExtended.Business.Services
+{
+    public class RecipeService : IRecipeService
+    {
+        public bool AddRecipe(RecipeDto recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteRecipe(Guid recipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RecipeDto GetRecipeById(Guid recipeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RecipeDto GetRecipeByTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RecipeDto> GetRecipesByCategory(List<CategoryDto> categories)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateRecipe(RecipeDto recipe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
