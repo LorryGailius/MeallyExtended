9c2f53974244126afc6a99790ec564ed1fc8c63c
Vladimir Deriugin <Vladimir.Deriugin.it@gmail.com>
2024-05-06 21:59:50 +0300

    Mapper method definition

--- MeallyExtended.Business/Mappers/MeallyMapper.cs
+++ MeallyExtended.Business/Mappers/MeallyMapper.cs
@@ -19,12 +19,14 @@ namespace MeallyExtended.Business.Mappers
             var configuration = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Category, CategoryDto>();
+                cfg.CreateMap<CategoryDto, Category>();
                 cfg.CreateMap<Ingredient, IngredientDto>();
+                cfg.CreateMap<IngredientDto, Ingredient>();
 
                 cfg.CreateMap<Review, ReviewDto>()
                .ForMember(dest => dest.UserEmail, opt => opt.MapFrom(src => src.User.Email));
-
-
+                cfg.CreateMap<ReviewDto, Review>()
+               .ForMember(dest => dest.User.Email, opt => opt.MapFrom(src => src.UserEmail));
 
 
                 cfg.CreateMap<Recipe, RecipeDto>()
@@ -34,10 +36,31 @@ namespace MeallyExtended.Business.Mappers
                  .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.Ingredients))
                  .ForMember(dest => dest.Reviews, opt => opt.MapFrom(src => src.Reviews));
 
+                cfg.CreateMap<RecipeDto, Recipe>()
+                .ForMember(dest => dest.Categories, opt => opt.Ignore())  // Assuming Category cannot be simply reconstructed
+                .ForMember(dest => dest.User, opt => opt.Ignore())        // Assuming User cannot be simply reconstructed
+                .ForMember(dest => dest.RecipeLikes, opt => opt.Ignore()) // Assuming RecipeLikes cannot be reconstructed directly
+                .ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.Ingredients))
+                .ForMember(dest => dest.Reviews, opt => opt.MapFrom(src => src.Reviews));
 
             });
 
             _mapper = configuration.CreateMapper();
+        }
+
+        public CategoryDto MapCategoryToDto(Category category)
+        {
+            return _mapper.Map<CategoryDto>(category);
+        }
+
+        public Category MapCategoryToEntity(CategoryDto categoryDto)
+        {
+            return _mapper.Map<Category>(categoryDto);
+        }
+
+        public Ingredient MapIngredientToEntity(IngredientDto ingredientDto)
+        {
+            return _mapper.Map<Ingredient>(ingredientDto);
         }
 
         public IngredientDto MapIngredientToDto(Ingredient ingredient)
@@ -50,9 +73,19 @@ namespace MeallyExtended.Business.Mappers
             return _mapper.Map<ReviewDto>(review);
         }
 
+        public Review MapReviewToEntity(ReviewDto reviewDto)
+        {
+            return _mapper.Map<Review>(reviewDto);
+        }
+
         public RecipeDto MapRecipeToDto(Recipe recipe)
         {
             return _mapper.Map<RecipeDto>(recipe);
+        }
+
+        public Recipe MapRecipeToEntity(RecipeDto recipeDto)
+        {
+            return _mapper.Map<Recipe>(recipeDto);
         }
     }
 
