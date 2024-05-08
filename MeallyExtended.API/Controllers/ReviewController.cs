13407ad4428563d67b1a2f2dd8886e119a13b66e
Gustas <loggustas@gmail.com>
2024-05-08 22:55:41 +0300

    Define ReviewController

--- MeallyExtended.API/Controllers/ReviewController.cs
+++ MeallyExtended.API/Controllers/ReviewController.cs
@@ -1,6 +1,41 @@
-﻿namespace MeallyExtended.API.Controllers;
+﻿using MeallyExtended.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
-public class ReviewController
+namespace MeallyExtended.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReviewController : ControllerBase
 {
-    
+    private readonly IReviewService _reviewService;
+
+    public ReviewController(IReviewService reviewService)
+    {
+        _reviewService = reviewService;
+    }
+
+    [HttpGet]
+    public IActionResult GetReviews()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public IActionResult AddReview() 
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut]
+    public IActionResult UpdateReview()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete]
+    public IActionResult DeleteReview() 
+    {
+        throw new NotImplementedException();
+    }
+
 }
