dc1c5b270491cb0a4920a75796a0223674daeceb
Gustas <loggustas@gmail.com>
2024-05-09 22:42:40 +0300

    Review controller

--- MeallyExtended.API/Controllers/ReviewController.cs
+++ MeallyExtended.API/Controllers/ReviewController.cs
@@ -1,5 +1,11 @@
-﻿using MeallyExtended.Business.Interfaces;
+﻿using Azure.Core;
+using MeallyExtended.Business.Interfaces;
+using MeallyExtended.Business.Mappers;
+using MeallyExtended.Contracts.Dto;
+using MeallyExtended.Contracts.Requests.Review;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace MeallyExtended.API.Controllers;
 
@@ -15,27 +21,78 @@ public class ReviewController : ControllerBase
     }
 
     [HttpGet]
-    public IActionResult GetReviews()
+    public async Task<IActionResult> GetReviews([FromRoute] Guid recipeId, [FromQuery] int limit = 5, [FromQuery] int skip = 0)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var reviews =  await _reviewService.GetLimitedReviews(recipeId, limit, skip);
+
+            ICollection<ReviewDto> result = new List<ReviewDto>();
+            foreach(var review in reviews)
+            {
+                result.Add(MeallyMapper.ReviewToDto(review));
+            }
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 
     [HttpPost]
-    public IActionResult AddReview() 
+    [Authorize]
+    public async Task<IActionResult> CreateReview([FromBody] CreateReviewRequest request) 
     {
-        throw new NotImplementedException();
+        try
+        {
+            var userEmail = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+
+            var review = MeallyMapper.CreateReviewRequestToReviewDto(request, userEmail);
+            var result = await _reviewService.AddReview(review);
+
+            return Ok(MeallyMapper.ReviewToDto(result));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 
     [HttpPut]
-    public IActionResult UpdateReview()
+    [Authorize]
+    public async Task<IActionResult> UpdateReview([FromBody] UpdateReviewRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var userEmail = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+
+            var result = await _reviewService.UpdateReview(request, userEmail);
+            return Ok(MeallyMapper.ReviewToDto(result));
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 
     [HttpDelete]
-    public IActionResult DeleteReview() 
+    [Authorize]
+    public async Task<IActionResult> DeleteReview([FromHeader] Guid reviewId) 
     {
-        throw new NotImplementedException();
+        try
+        {
+            var userEmail = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+            var result = await _reviewService.DeleteReview(reviewId, userEmail);
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest();
+        }
+
     }
 
 }
