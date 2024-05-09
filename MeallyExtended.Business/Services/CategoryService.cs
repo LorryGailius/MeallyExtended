f6d34b7bda81776bff647c30370804431a5b1d31
Neringa <maj.neringa@gmail.com>
2024-05-09 18:35:07 +0300

    define category service body

--- MeallyExtended.Business/Services/CategoryService.cs
+++ MeallyExtended.Business/Services/CategoryService.cs
@@ -1,7 +1,26 @@
-﻿namespace MeallyExtended.Business.Services
+﻿using MeallyExtended.Business.Interfaces;
+using MeallyExtended.Data.Repositories.Interfaces;
+using MeallyExtended.DataModels.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace MeallyExtended.Business.Services
 {
-    public class CategoryService
+    public class CategoryService : ICategoryService
     {
-    
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository ?? throw new ArgumentNullException(nameof(categoryRepository));
+        }
+
+        public async Task<Category?> GetCategoryByName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Category name cannot be empty.", nameof(name));
+
+            return await _categoryRepository.GetCategoryByName(name);
+        }
     }
 }
