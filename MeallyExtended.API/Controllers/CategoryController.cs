63879c1d7b0e8535b1148375e51b12ce49638cca
Neringa <neringa.majauskaite@mif.stud.vu.lt>
2024-05-09 20:47:15 +0300

    created category controller

---
+++ MeallyExtended.API/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using MeallyExtended.Business.Interfaces;
+using MeallyExtended.Business.Mappers;
+using MeallyExtended.Contracts.Requests.Category;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeallyExtended.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("{categoryName}")]
+        public async Task<IActionResult> GetCategory(string categoryName)
+        {
+            var category = await _categoryService.GetCategoryByName(categoryName);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
+    }
+}
