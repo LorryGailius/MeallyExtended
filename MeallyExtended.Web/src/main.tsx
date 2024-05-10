a388e94a1cfb33b0ffe14760bb1c12d8f2859579
Laurynas Gailius <laurynas.gailius@twoday.com>
2024-05-10 09:50:31 +0300

    Added tailwindCSS, ShadcnUI and React-Router to the frontend project with a few sample pages

--- MeallyExtended.Web/src/main.tsx
+++ MeallyExtended.Web/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import IndexPage from './pages/IndexPage.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './pages/ErrorPage.tsx';
+
+const routes = createBrowserRouter([
+  {
+    // This is the index page
+    path: "/",
+    element: <IndexPage />,
+  },
+  {
+    // This is the 404 page
+    path: "*",
+    element: <ErrorPage />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
