1791e3dbb5275d51a73c9818c0a699c709d26b8b
Neringa <neringa.majauskaite@mif.stud.vu.lt>
2024-05-23 21:23:49 +0300

    recipe grid for home and browse pages

--- MeallyExtended.Web/src/pages/HomePage.tsx
+++ MeallyExtended.Web/src/pages/HomePage.tsx
@@ -1,18 +1,9 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useRecipesBrowse } from "@/hooks/useRecipesBrowse";
-import { Button } from "@/components/ui/button";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
 import { useRecipesPopular } from "@/hooks/useRecipesPopular";
 import { Link } from "react-router-dom";
 import Header from "@/components/ui/header";
+import RecipeGrid from "../components/ui/RecipeGrid";
 
 const HomePage: React.FC = () => {
   const {
@@ -20,7 +11,6 @@ const HomePage: React.FC = () => {
     loading: browseLoading,
     error: browseError,
     recipes,
-    totalPages,
   } = useRecipesBrowse();
   const {
     fetchPopularRecipes,
@@ -28,56 +18,26 @@ const HomePage: React.FC = () => {
     error: popularError,
     popularRecipes,
   } = useRecipesPopular();
-  const [page, setPage] = useState(1);
+  const [page] = useState(1);
   const [pageSize] = useState(15);
-  const history = useNavigate();
 
   useEffect(() => {
     browseRecipes(page, pageSize);
     fetchPopularRecipes(5);
   }, [page, pageSize]);
 
-  // if (browseLoading || popularLoading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (browseLoading || popularLoading) {
+    return <div>Loading...</div>;
+  }
 
-  // if (browseError || popularError) {
-  //   return <div>Error: {browseError || popularError}</div>;
-  // }
+  if (browseError || popularError) {
+    return <div>Error: {browseError || popularError}</div>;
+  }
 
   return (
     <>
       <div id="index-page" className="h-screen">
         <Header />
-        {/* <div
-          className="flex justify-between items-center"
-          style={{ padding: 40 }}
-        >
-          <div>
-            <h1 className="text-4xl font-bold">Meally</h1>
-            <p className="text-lg">Your meal planner</p>
-          </div>
-          <div>
-            <Button
-              className="bg-primary-background mr-2"
-              onClick={() => history("/create-recipe")}
-            >
-              Create New Recipe
-            </Button>
-            <Button
-              className="mr-2 bg-accent"
-              onClick={() => history("/login")}
-            >
-              Login
-            </Button>
-            <Button
-              className="mr-2 bg-accent"
-              onClick={() => history("/register")}
-            >
-              Register
-            </Button>
-          </div>
-        </div>
         <div
           className="flex flex-col gap-4 justify-center bg-primary-background items-center"
           style={{ padding: 40 }}
@@ -89,11 +49,21 @@ const HomePage: React.FC = () => {
             {popularRecipes.map((recipe) => (
               <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
                 <div className="flex flex-col items-center">
-                  <div>
+                  <div
+                    style={{
+                      height: "100px",
+                      width: "100px",
+                      overflow: "hidden",
+                    }}
+                  >
                     <img
-                      src={recipe.image || "https://via.placeholder.com/150"}
+                      src={recipe.imageUrl || "https://via.placeholder.com/150"}
                       alt={recipe.title}
-                      style={{ maxWidth: "100%", maxHeight: "100%" }}
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                      }}
                     />
                   </div>
                   <p className="text-sm">{recipe.duration} minutes</p>
@@ -113,60 +83,11 @@ const HomePage: React.FC = () => {
           style={{ padding: 40 }}
         >
           <h2 className="text-3xl font-regular">All recipes</h2>
-          <div className="grid grid-cols-5 gap-4">
-            {recipes.map((recipe) => (
-              <Link to={`/recipes/${recipe.id}`} key={recipe.id}>
-                <div className="flex flex-col items-center p-4">
-                  <div>
-                    <img
-                      src={recipe.image || "https://via.placeholder.com/150"}
-                      alt={recipe.title}
-                      style={{ maxWidth: "100%", maxHeight: "100%" }}
-                    />
-                  </div>
-                  <p className="text-sm">{recipe.duration} minutes</p>
-                  <p className="text-sm">
-                    {recipe.categories.length !== 0
-                      ? recipe.categories[0].Name
-                      : ""}
-                  </p>
-                  <p className="text-lg">{recipe.title}</p>
-                </div>
-              </Link>
-            ))}
-          </div>
+          <RecipeGrid recipes={recipes} columns={5} />
+          <Link to="/recipes" className="mt-4 text-center">
+            Browse All Recipes
+          </Link>
         </div>
-        {totalPages > 1 && (
-          <Pagination>
-            <PaginationContent>
-              <PaginationItem>
-                <PaginationPrevious
-                  href="#"
-                  onClick={() => setPage((prev) => Math.max(prev - 1, 1))}
-                />
-              </PaginationItem>
-              {Array.from({ length: totalPages }, (_, i) => (
-                <PaginationItem key={i}>
-                  <PaginationLink
-                    href="#"
-                    isActive={i + 1 === page}
-                    onClick={() => setPage(i + 1)}
-                  >
-                    {i + 1}
-                  </PaginationLink>
-                </PaginationItem>
-              ))}
-              <PaginationItem>
-                <PaginationNext
-                  href="#"
-                  onClick={() =>
-                    setPage((prev) => Math.min(prev + 1, totalPages))
-                  }
-                />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
-        )} */}
       </div>
     </>
   );
